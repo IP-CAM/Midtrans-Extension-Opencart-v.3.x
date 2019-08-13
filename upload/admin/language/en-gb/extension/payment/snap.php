efe4681d169e54b6668d5164c25b76a40f8d956d
HP Web Design <51750567+hpwebdesign@users.noreply.github.com>
2019-08-13 21:12:29 +0700

    initial upload
    initial upload

---
+++ upload/admin/language/en-gb/extension/payment/snap.php
@@ -0,0 +1,51 @@
+<?php
+// Heading
+$_['heading_title']          = '<span style="border-radius:2px;color:#fff;background:#8fbb6c;padding:2px 5px;">HP</span> Snap';
+
+$_['heading_title2']         = 'Snap';
+
+// Text
+$_['text_extension']        = 'Extensions';
+$_['text_payment'] = 'Payment';
+$_['text_success'] = 'Success: You have modified Snap configuration!';
+$_['text_snap'] = '<a href="https://midtrans.com" target="_blank"><img src="view/image/payment/midtrans.png" width="120px" alt="Midtrans" title="Snap" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_live'] = 'production';
+$_['text_sandbox'] = 'sandbox';
+$_['text_successful'] = 'Always Successful';
+$_['text_fail'] = 'Always Fail';
+$_['text_edit'] = 'Configure Midtrans Snap';
+
+// Entry
+$_['entry_environment'] = 'Environment'; // v2 API only
+$_['entry_merchant_id'] = 'Merchant Id'; // v2 API only
+$_['entry_client_key'] = 'Client Key'; // v2 API only
+$_['entry_server_key'] = 'Server Key'; // v2 API only
+$_['entry_order_status'] = 'Order Status';
+$_['entry_oneclick'] = 'One click';
+$_['entry_geo_zone'] = 'Geo Zone';
+$_['entry_status'] = 'Status';
+$_['entry_sort_order'] = 'Sort Order:';
+$_['entry_expiry'] = 'Custom Expire';
+$_['entry_custom_field'] = 'Custom Field';
+$_['entry_currency_conversion'] = 'Currency conversion to IDR';
+
+// HPWD
+$_['entry_snap_success_mapping']    = 'Success Order Status';
+$_['help_snap_success_mapping']     = 'Change to the following order status once the payment success';
+$_['entry_snap_challenge_mapping']  = 'Pending Order Status';
+$_['help_snap_challenge_mapping']   = 'Change to the following order status once the payment pending';
+$_['entry_snap_failure_mapping']    = 'Failure Order Status';
+$_['help_snap_failure_mapping']     = 'Change to the following order status once the payment failure';
+
+
+$_['entry_snap_failure_mapping'] = 'Map Payment Failure Status to Order Status:';
+$_['entry_display_name'] = 'Display name:';
+$_['entry_status_success'] = 'Success Order Status';
+$_['entry_status_failed'] = 'Failure Order Status';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Snap!';
+$_['error_client_key'] = 'Client Key is required!';
+$_['error_server_key'] = 'Server Key is required!';
+$_['error_currency_conversion'] = 'Currency conversion rate is required when IDR currency is not installed in the system!';
+$_['error_display_name'] = 'Please specify a name for this payment method!';
